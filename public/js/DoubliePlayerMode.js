f3661e8ee0f267bbadd893a69f773e8b170ea007
SangLiang <378305868@qq.com>
2017-01-23 14:13:17 +0800

    changed some mode

---
+++ public/js/DoubliePlayerMode.js
@@ -0,0 +1,56 @@
+// 双人模式逻辑
+var UI = require("./UI.js");
+var util = require("./util.js");
+var gameLogic = require('./gameLogic.js');
+
+var gameList = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+];
+
+var gameTurn = 0; // 0代表黑子，1代表白子
+var result = false;
+
+module.exports.start = function () {
+    UI.changedSideText(gameTurn);
+    UI.background.isTrigger = true;
+    Hamster.addEventListener(UI.background, "click", function (e) {
+        console.log(result);
+        if(result){
+            return;
+        }
+
+        var position = util.getBoardPosition(e.x, e.y);
+        var _pos = util.setPositionByBoardPosition(position.x, position.y);
+        var rightPlace = gameLogic.setPieceInGameList(gameTurn, gameList, position);
+        if (!rightPlace) {
+            return;
+        }
+        //生成棋子 
+        var piece = gameLogic.shotPiece(gameTurn, _pos);
+        Hamster.add(piece);
+        result = gameLogic.getResult(gameList, position.x, position.y);
+        if (gameTurn == 0) {
+            gameTurn = 1;
+        } else {
+            gameTurn = 0;
+        }
+        UI.changedSideText(gameTurn);
+    });
+
+    Hamster.add(UI.background);
+    Hamster.add(UI.turnUI);
+}
