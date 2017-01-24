f6954abbb30f63ac3bde45ea1027695114f4055f
SangLiang <378305868@qq.com>
2017-01-24 16:59:10 +0800

    some changed

--- public/js/AI.js
+++ public/js/AI.js
@@ -1,24 +1,53 @@
 // AI部分
 
-function AI(){
+function AI() {
     var self = this;
+    self.horizontalList = [];
+    self.verticaList = [];
+    self.viceDiagonalList = [];
+    self.mainDiagonalList = [];
     self.init();
 }
 
-AI.prototype ={
-    "init":function(){
+AI.prototype = {
+    "init": function () {
         console.log("AI ready");
     },
 
-    "checkGameList":function(gameList){
-        for(var i = 0; i<gameList.length;i++){
-            for(var j = 0; j< gameList[i].length;j++){
-                
+    "checkGameList": function (gameList) {
+        for (var i = 0; i < gameList.length; i++) {
+            for (var j = 0; j < gameList[i].length; j++) {
+                // 横向检测条件
+                if (x <= 10 && gameList[i][j] != 0) {
+                    this.checkHorizontal(gameList, i, j);
+                }
             }
         }
     },
     // 横排检测
-    "checkHorizontal":function(){
+    "checkHorizontal": function (gameList, i, j) {
+        var _obj = {};
+        if (gameList[i][j] == gameList[i + 1][j]) {
+            _obj.x = i;
+            _obj.y = j;
+            this.horizontalList.push(_obj);
+        }
+    },
+    // 竖排检测
+    "checkVertica": function () {
 
+    },
+    // 副对角线
+    "checkViceDiagonal": function () {
+
+    },
+    // 主对角线
+    "checkMainDiagonal": function () {
+
+    },
+    "shotPiece": function () {
+        console.log(1);
     }
 }
+
+module.exports = AI;
--- public/js/SingleMode.js
+++ public/js/SingleMode.js
@@ -2,6 +2,7 @@
 var UI = require("./UI.js");
 var util = require("./util.js");
 var gameLogic = require('./gameLogic.js');
+var AI = require("./AI");
 
 var gameList = [
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -24,11 +25,16 @@ var gameList = [
 var gameTurn = 0; // 0代表黑子，1代表白子
 
 module.exports.start = function () {
+    var ai = new AI();
+    // 设置电脑的先后手
     gameTurn = util.getRandomNumber(0, 1);
     UI.changedSideText(gameTurn);
+
+    if (gameTurn == 0){
+        ai.shotPiece();
+    }
 
 
     Hamster.add(UI.background);
     Hamster.add(UI.turnUI);
-    console.log(gameTurn);
 }
